9d6b1ea7309b090d8ef22c6c2a0c69261e5b9494
Nguyen-Phu-B <nguyenhphub@gmail.com>
2023-04-16 14:10:17 +0700

    ---

--- script/loadProduct.js
+++ script/loadProduct.js
@@ -5,40 +5,6 @@ let $sel = document.getElementById("renderIphones");
 
 let _render = '';
 
-// for (let i = data_iphone.length - 1; i > 0; i--) {
-// for (let i = 0; i < data_iphone.length; i++) { 
-//     let title = `${data_iphone[i].model} ${data_iphone[i].color[0]} ${data_iphone[i].memory[0]} gb`;
-//     let uppercaseTitle = title.toUpperCase();
-//     _render += `<div class="box-product">
-//                 <div class="box-img">
-//                     <a href="">
-//                         <img src=".${data_iphone[i].imgProduct[0]}" alt="">
-//                     </a>
-//                 </div>
-//                 <div class="box-title">
-//                     ${uppercaseTitle}
-//                 </div>
-//                 <div class="box-price">
-//                     <div class="price-actual">
-//                         ${data_iphone[i].actualPrice[0]}
-//                     </div>
-//                     <div class="price-old">
-//                         ${data_iphone[i].oldPrice[0]}
-//                     </div>
-//                 </div>
-//                 <div class="btn-add-cart">
-//                     <a  class="box-add-cart" href="">
-//                         <div class="box-add-btn">
-//                             Add to cart
-//                         </div>
-//                         <div class="box-icon">
-//                             <i class="fa-solid fa-cart-arrow-down fa-xl"></i>
-//                         </div>
-//                     </a>
-//                 </div>
-//             </div> `
-// }
-
 for (let i = 0; i < data_iphone.length; i++) {
     let color = data_iphone[i].color && data_iphone[i].color.length > 0 ? data_iphone[i].color[0] : '';
     let memory = data_iphone[i].memory && data_iphone[i].memory.length > 0 ? data_iphone[i].memory[0] : '';
@@ -78,39 +44,6 @@ for (let i = 0; i < data_iphone.length; i++) {
             </div> `;
 }
     
-    // let title = `${data_iphone[i].model} ${data_iphone[i].color[0]} ${data_iphone[i].memory[0]} gb`;
-    // let uppercaseTitle = title.toUpperCase();
-
-
-        // _render += `<div class="box-product">
-        //         <div class="box-img">
-        //             <a href="">
-        //                 <img src="./img/iphone/iphone14pro/iphone_14_pro_gold.png" alt="">
-        //             </a>
-        //         </div>
-        //         <div class="box-title">
-        //             ${uppercaseTitle}
-        //         </div>
-        //         <div class="box-price">
-        //             <div class="price-actual">
-        //                 ${data_iphone[i].actualPrice[0]}
-        //             </div>
-        //             <div class="price-old">
-        //                 ${data_iphone[i].oldPrice[0]}
-        //             </div>
-        //         </div>
-        //         <div class="btn-add-cart">
-        //             <a  class="box-add-cart" href="">
-        //                 <div class="box-add-btn">
-        //                     Add to cart
-        //                 </div>
-        //                 <div class="box-icon">
-        //                     <i class="fa-solid fa-cart-arrow-down fa-xl"></i>
-        //                 </div>
-        //             </a>
-        //         </div>
-        //     </div> `
-        
 $sel.innerHTML = _render;
 
 var links = document.querySelectorAll('.box-add-cart');
